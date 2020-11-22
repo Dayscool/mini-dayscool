8fecf30b9415c398a8f10f3eb94a8d77cfc8f000
EC2 Default User <ec2-user@ip-172-31-66-145.ec2.internal>
2020-11-22 07:58:19 +0000

    work

--- soap/app/controllers/interface_controller.rb
+++ soap/app/controllers/interface_controller.rb
@@ -4,10 +4,10 @@ require 'json'
 class InterfaceController < ApplicationController
   soap_service namespace: 'urn:WashOut'
 
-  soap_action "integer_to_string",
+  soap_action "profesores",
               :args   => nil,
               :return => [{:name => :string, :mail => :string}]
-  def integer_to_string
+  def profesores
     response = HTTParty.post("http://172.17.0.1:5000/graphql", body: {"query":"{\n  getTeachers{\n    name\n    mail\n  }\n}",variables:"null",operationName:nil})
     print(response.body)
     render :soap => JSON.parse(response.body)["data"]["getTeachers"]
